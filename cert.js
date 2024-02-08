f307aa6b41d0b70137454d67e46c3c7e4d48967d
K6id3n <kaidengarza203@gmail.com>
2024-02-08 10:44:27 -0800

    JS

--- cert.js
+++ cert.js
@@ -23,6 +23,14 @@ function getCertificate() {
     var output3 = document.getElementById("input3").value;
     var output4 = document.getElementById("input4").value;
 
-   
+    const nameFinal = document.getElementById("nametext");
+    const awardFinal = document.getElementById("awardtext");
+    const issueFinal = document.getElementById("issuetext");
+    const dateFinal = document.getElementById("datetext");
+
+    nameFinal.textContent = output1;
+    awardFinal.textContent = output2;
+    issueFinal.textContent = output3;
+    dateFinal.textContent = output4;
 
 }
